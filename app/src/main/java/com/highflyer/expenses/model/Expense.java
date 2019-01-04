f9517354c54cb2c1126381327860061e874eee0a
VladMihai28 <mihai.vlad28@gmail.com>
2019-01-04 09:52:59 +0200

    The Expenses class now implements Parcelable and has a new builder associated with it

--- app/src/main/java/com/highflyer/expenses/model/Expense.java
+++ app/src/main/java/com/highflyer/expenses/model/Expense.java
@@ -1,18 +1,67 @@
 package com.highflyer.expenses.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Vlad
  */
 
-public class Expense {
+public class Expense implements Parcelable {
 
     private float value;
     private int date;
     private String category;
 
-    public Expense(float value, int date, String category){
+    public Expense(){}
+
+    public Expense(Parcel parcel){
+        this.value = parcel.readFloat();
+        this.date = parcel.readInt();
+        this.category = parcel.readString();
+    }
+
+    public Expense(ExpenseBuilder builder){
+        value = builder.value;
+        date = builder.date;
+        category = builder.category;
+    }
+
+    public class ExpenseBuilder{
+
+        private float value;
+        private int date;
+        private String category;
+
+        public ExpenseBuilder(){}
+
+        public ExpenseBuilder value(float newValue){
+            value = newValue;
+            return this;
+        }
+        public ExpenseBuilder date(int newDate){
+            date = newDate;
+            return this;
+        }
+        public ExpenseBuilder category(String newCategory){
+            category = newCategory;
+            return this;
+        }
+
+        public Expense build(){
+            return new Expense(this);
+        }
+    }
+
+    public void setValue(float value) {
         this.value = value;
+    }
+
+    public void setDate(int date) {
         this.date = date;
+    }
+
+    public void setCategory(String category) {
         this.category = category;
     }
 
@@ -27,4 +76,29 @@ public class Expense {
     public String getCategory() {
         return category;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeFloat(value);
+        parcel.writeInt(date);
+        parcel.writeString(category);
+    }
+
+    public static final Parcelable.Creator<Expense> CREATOR = new Parcelable.Creator<Expense>() {
+
+        @Override
+        public Expense createFromParcel(Parcel parcel) {
+            return new Expense(parcel);
+        }
+
+        @Override
+        public Expense[] newArray(int size) {
+            return new Expense[size];
+        }
+    };
 }

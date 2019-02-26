0cbf34e885c01251ee075f5640d28f0215238b99
VladMihai28 <mihai.vlad28@gmail.com>
2019-02-26 21:35:55 +0200

    Fix some of the Sonar code smells

--- app/src/main/java/com/highflyer/expenses/data/ExpenseContract.java
+++ app/src/main/java/com/highflyer/expenses/data/ExpenseContract.java
@@ -10,6 +10,11 @@ public class ExpenseContract {
     private ExpenseContract(){}
 
     public static final class ExpenseEntry implements BaseColumns {
+
+        private ExpenseEntry() {
+            throw new IllegalStateException("DB Contract Class");
+        }
+
         public static final String TABLE_NAME = "expenses";
         public static final String COLUMN_VALUE = "value";
         public static final String COLUMN_CURRENCY = "currency";

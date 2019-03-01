858993535a9609227fa138ad22ee7c6184394df0
VladMihai28 <mihai.vlad28@gmail.com>
2019-03-01 07:34:30 +0200

    Fix Codacy issues

--- app/src/main/java/com/highflyer/expenses/data/ExpenseContract.java
+++ app/src/main/java/com/highflyer/expenses/data/ExpenseContract.java
@@ -11,15 +11,15 @@ public class ExpenseContract {
 
     public static final class ExpenseEntry implements BaseColumns {
 
-        private ExpenseEntry() {
-            throw new IllegalStateException("DB Contract Class");
-        }
-
         public static final String TABLE_NAME = "expenses";
         public static final String COLUMN_VALUE = "value";
         public static final String COLUMN_CURRENCY = "currency";
         public static final String COLUMN_TRANSACTION_DATE = "date";
         public static final String COLUMN_CATEGORY = "category";
+
+        private ExpenseEntry() {
+            throw new IllegalStateException("DB Contract Class");
+        }
 
     }
 

f1db2842ec2f48029a27d0d8c37b35382dd5e5a0
VladMihai28 <mihai.vlad28@gmail.com>
2019-03-01 07:49:16 +0200

    Refactoring transform Anonymous class into lambda

--- app/src/main/java/com/highflyer/expenses/MainActivity.java
+++ app/src/main/java/com/highflyer/expenses/MainActivity.java
@@ -2,7 +2,6 @@ package com.highflyer.expenses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -18,10 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void initializeAddButton(){
         Button addButton = findViewById(R.id.addExpenseButton);
-        addButton.setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v){
-                Toast.makeText(getApplicationContext(), "Added an expense", Toast.LENGTH_SHORT).show();
-            }
-        });
+        addButton.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Added an expense", Toast.LENGTH_SHORT).show());
     }
 }
